b94d8a772afada7d8ed56a3def27577209cf5cf6
Tõnu Samuel <tonu@jes.ee>
2011-06-06 00:30:01 +0300

    Made work on vision publisher. Test messages are OK now

--- tyros_camera/src/tyros_cam.cpp
+++ tyros_camera/src/tyros_cam.cpp
@@ -22,6 +22,7 @@
 #include <string>
 
 #include <ros/ros.h>
+#include <tyros_camera/Objects.h>
 #include <sensor_msgs/CameraInfo.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/image_encodings.h>
@@ -32,7 +33,6 @@
 #include "opencv2/highgui/highgui_c.h"
 #include "opencv2/legacy/compat.hpp"
 #include "opencv2/objdetect/objdetect.hpp"
-
 
 #include "libcam.h"
 #include "image.h"
@@ -69,11 +69,16 @@ int main(int argc, char **argv) {
 	image.width = 240;
 	image.encoding = sensor_msgs::image_encodings::MONO8;
 
+        ros::init(argc, argv, "TIChronos_teleop");
+        ros::NodeHandle n;
+        ros::Rate loop_rate(60);
+        ros::Publisher pub;
+
+        pub = n.advertise<tyros_camera::Objects>("vision_publisher", 5); /* Message queue length is 3 */
 
 	IplImage* imgy;
         imgy= cvCreateImage(cvSize(IMAGE_WIDTH,IMAGE_HEIGHT), 8, 1);
-	    cvNamedWindow( "result", 1 );
-	//cvZero(imgy);
+	cvNamedWindow( "result", 1 );
         cvStartWindowThread();
 	while (ros::ok()) {
 		unsigned char* ptr = cam.Update();
@@ -83,6 +88,7 @@ int main(int argc, char **argv) {
 		int image_size = 320*240;
 		image.step = 240;
 		image.data.resize(image_size);
+                tyros_camera::Objects msg;
 
 		// For each input pixel we do have 2 bytes of data. But we read them in groups of four because of YUYV format
         	for(i=0,j=0;i<(IMAGE_WIDTH*IMAGE_HEIGHT*2) ; i+=2,j+=1) {
@@ -103,7 +109,6 @@ int main(int argc, char **argv) {
 		find_circles(imgy);
 //		draw_circles(imgy)
 
-
                 double hScale=0.7;
                 double vScale=0.7;
                 int    lineWidth=1;
@@ -111,6 +116,16 @@ int main(int argc, char **argv) {
                 cvInitFont(&font,CV_FONT_HERSHEY_SIMPLEX, hScale,vScale,0,lineWidth,0);
                 for (i=0; circles[i].x_in_picture != -1; i++) {
                     char buf[256];
+                    tyros_camera::Object o;
+
+                    o.vector.x=circles[i].x_in_picture;
+                    o.vector.y=circles[i].y_in_picture;
+                    o.vector.z=i;
+                    o.certainity=i;
+                    o.type="Ball";
+                    msg.object.push_back(o);
+
+
 #ifdef DEBUG
                     printf("Circle[%d] x:%d y:%d r:%d\n", i, circles[i].x_in_picture, circles[i].y_in_picture, circles[i].r_in_picture);
                     printf("Circle[%d] real: x:%lf y:%lf r:%lf\n", i, circles[i].x_from_robot, circles[i].y_from_robot, circles[i].r_from_robot);
@@ -126,6 +141,8 @@ int main(int argc, char **argv) {
                 }
 
 
+                pub.publish(msg);
+           //     msg.object.clear();
                 cvShowImage( "result", imgy );
 //		cam_info = cinfo.getCameraInfo();
   	        cam_info.header.frame_id = image.header.frame_id;
