098bd9bfb351e37fa02bb2642e5312ef45cbc32e
Tonu Samuel <tonu@jes.ee>
2012-02-23 22:00:56 +0900

    histogram is shown now too

--- tyros_camera/src/tyros_cam.cpp
+++ tyros_camera/src/tyros_cam.cpp
@@ -125,6 +125,8 @@ int main(int argc, char **argv) {
         imgy= cvCreateImage(cvSize(IMAGE_WIDTH,IMAGE_HEIGHT), 8, 1);
         imgu= cvCreateImage(cvSize(IMAGE_WIDTH,IMAGE_HEIGHT), 8, 1);
         imgv= cvCreateImage(cvSize(IMAGE_WIDTH,IMAGE_HEIGHT), 8, 1);
+        IplImage* planes[] = { imgy, imgu};
+
 #ifdef DEBUG
 	cvNamedWindow( "result Y", 0 );
 	cvNamedWindow( "result U", 0 );
@@ -180,6 +182,65 @@ int main(int argc, char **argv) {
         	}
 		find_circles(imgy);
 	//	draw_circles(imgy);
+
+
+
+
+        // Build the histogram and compute its contents.
+        //
+        int h_bins = 30, s_bins = 32; 
+        CvHistogram* hist;
+        {
+          int    hist_size[] = { h_bins, s_bins };
+          float  h_ranges[]  = { 0, 180 };          // hue is [0,180]
+          float  s_ranges[]  = { 0, 255 }; 
+          float* ranges[]    = { h_ranges, s_ranges };
+          hist = cvCreateHist( 
+            2, 
+            hist_size, 
+            CV_HIST_ARRAY, 
+            ranges, 
+            1 
+          ); 
+        }
+        cvCalcHist( planes, hist, 0, 0 );
+
+        // Create an image to use to visualize our histogram.
+        //
+        int scale = 10;
+        IplImage* hist_img = cvCreateImage(  
+          cvSize( h_bins * scale, s_bins * scale ), 
+          8, 
+          3
+        ); 
+        cvZero( hist_img );
+
+        // populate our visualization with little gray squares.
+        //
+        float max_value = 0;
+        cvGetMinMaxHistValue( hist, 0, &max_value, 0, 0 );
+
+        for( int h = 0; h < h_bins; h++ ) {
+            for( int s = 0; s < s_bins; s++ ) {
+                float bin_val = cvQueryHistValue_2D( hist, h, s );
+                int intensity = cvRound( bin_val * 255 / max_value );
+                cvRectangle( 
+                  hist_img, 
+                  cvPoint( h*scale, s*scale ),
+                  cvPoint( (h+1)*scale - 1, (s+1)*scale - 1),
+                  CV_RGB(intensity,intensity,intensity), 
+                  CV_FILLED
+                );
+            }
+        }
+
+//        cvNamedWindow( "Source", 1 );
+//        cvShowImage(   "Source", src );
+
+        cvNamedWindow( "H-S Histogram", 1 );
+        cvShowImage(   "H-S Histogram", hist_img );
+
+
 
 #ifdef DEBUG
                 double hScale=0.7;
